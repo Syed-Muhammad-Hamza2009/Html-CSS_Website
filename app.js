a98aa0607d299ce87c12dbb46deec0981688bdab
Syed-Muhammad-Hamza2009 <hamzacamander@gmail.com>
2025-01-22 04:32:38 -0800

    i have done

---
+++ app.js
@@ -0,0 +1,4 @@
+var _a;
+(_a = document.getElementById('changeMessageButton')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    alert('Button clicked! You can replace this with any action you want.');
+});
---
+++ app.ts
@@ -0,0 +1,3 @@
+document.getElementById('changeMessageButton')?.addEventListener('click', () => {
+    alert('Button clicked! You can replace this with any action you want.');
+});
